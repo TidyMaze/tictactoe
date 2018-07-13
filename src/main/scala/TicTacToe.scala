da7d6d196c97a89713880c4fada8dd2575584551
Yann <groskiff@gmail.com>
2018-07-13 21:33:31 +0200

    monte carlo AI

--- src/main/scala/TicTacToe.scala
+++ src/main/scala/TicTacToe.scala
@@ -1,6 +1,9 @@
-import TicTacToe.Grid
+import java.security.InvalidParameterException
+
+import TicTacToe.Brain
 
 import scala.io.StdIn
+import scala.util.Random
 
 sealed abstract class Player(val symbol: String)
 case object PlayerX extends Player("X")
@@ -9,8 +12,11 @@ case object PlayerY extends Player("Y")
 
 object TicTacToe extends App {
   type Grid = Seq[Seq[Option[Player]]]
+  type Brain = (Seq[Coord], Grid, Player) => Coord
   case class Coord(x: Int, y: Int)
   val Dim = 3
+
+  val random = new Random()
 
   def showCell: Option[Player] => String = {
     case Some(p) => p.symbol
@@ -40,16 +46,26 @@ object TicTacToe extends App {
     case _ => None
   }
 
-  def getPlayerAction(availableCells: Seq[Coord], player: Player): Coord = {
+  def getHumanAction: Brain = { (availableCells, grid, player) =>
+    println("Current grid:")
+    printGrid(grid)
     val xy = StdIn.readLine(s"Action for player ${player.symbol}? ").split("").map(_.toInt)
     val picked = Coord(xy(0), xy(1))
     if (availableCells.contains(picked)) {
       picked
     } else {
       println(s"Cell $picked is not allowed: Not available.")
-      getPlayerAction(availableCells, player)
+      getHumanAction(availableCells, grid, player)
     }
   }
+
+  def randomIn[A](xs: Seq[A]): A =
+    if(xs.isEmpty)
+      throw new InvalidParameterException("Empty seq")
+     else
+      xs(random.nextInt(xs.size))
+
+  def getRandomAction: Brain = (availableCells, grid, player) => randomIn(availableCells)
 
   def nextPlayer: Player => Player = {
     case PlayerX => PlayerY
@@ -57,29 +73,55 @@ object TicTacToe extends App {
   }
 
   def availableCells(grid: Grid): Seq[Coord] = for {
-    x <- grid.head.indices
     y <- grid.indices
+    x <- grid.head.indices
     if grid(y)(x).isEmpty
   } yield Coord(x,y)
 
-  def stepOneTurn(currentPlayer: Player, currentGrid: Grid): Option[Player] = {
-    println("Current grid:")
-    printGrid(currentGrid)
+  def playTillEnd(currentPlayer: Player, currentGrid: Grid, getPlayerAction: Brain, getPlayerOppAction: Brain): (Option[Player], Grid) = {
     val possibleCells = availableCells(currentGrid)
-    val action = getPlayerAction(possibleCells, currentPlayer)
+    val action = getPlayerAction(possibleCells, currentGrid, currentPlayer)
     val nextGrid = putPlayer(currentGrid, currentPlayer, action)
     val maybeWinner = getWinner(nextGrid)
     val stillPlayable = availableCells(nextGrid).nonEmpty
     (maybeWinner, stillPlayable) match {
-      case (Some(winner), _) => Some(winner)
-      case (None, false) => None
-      case (None, true) => stepOneTurn(nextPlayer(currentPlayer), nextGrid)
+      case (Some(winner), _) => (Some(winner), nextGrid)
+      case (None, false) => (None, nextGrid)
+      case (None, true) => playTillEnd(nextPlayer(currentPlayer), nextGrid, getPlayerOppAction, getPlayerAction)
     }
   }
 
-  def fullGame(): Option[Player] = stepOneTurn(PlayerX, mkGrid(Dim))
+  def fullGame(player: Player, getPlayerAction: Brain, getPlayerOppAction: Brain, grid: Grid): (Option[Player], Grid) = playTillEnd(player, grid, getPlayerAction, getPlayerOppAction)
 
-  val maybeWinner = fullGame()
+  def monteCarlo(currentPlayer: Player, grid: Grid, nbGames: Int): Coord = {
+    val score: ((Coord, Seq[Option[Player]])) => Double = { entry =>
+        val wins = entry._2.count(maybeWinner => maybeWinner.isDefined && maybeWinner.contains(currentPlayer))
+        val loses = entry._2.count(maybeWinner => maybeWinner.isDefined && !maybeWinner.contains(currentPlayer))
+        val draws = entry._2.count(maybeWinner => maybeWinner.isEmpty)
+        (wins.toDouble * 1 + loses.toDouble * -1) / entry._2.size
+    }
+    availableCells(grid).map(rootAction => {
+      val endings = 0 until nbGames map { _ =>
+        fullGame(
+          nextPlayer(currentPlayer),
+          getRandomAction,
+          getRandomAction,
+          putPlayer(
+            grid,
+            currentPlayer,
+            rootAction
+          )
+        )._1
+      }
+      rootAction -> endings
+    }
+    ).toMap.toSeq.maxBy(score)._1
+  }
+
+  def monteCarloBrain: Brain = (availableCells, grid, player) => monteCarlo(player, grid, 1000)
+
+  val (maybeWinner, endingGrid) = fullGame(PlayerX, getHumanAction, monteCarloBrain, mkGrid(Dim))
   val endingInfo = maybeWinner.map(p => s"${p.symbol} WINS!").getOrElse("IT'S A DRAW!")
   println(endingInfo)
+  printGrid(endingGrid)
 }
